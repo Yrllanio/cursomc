95ed42ac27bf501ea769b8f8a722084b0d7bc4ba
yrllanio <yrlanio@gmail.com>
2020-10-15 20:42:58 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/yrllanio/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/yrllanio/cursomc/dto/CategoriaDTO.java
@@ -4,18 +4,20 @@ import java.io.Serializable;
 
 import com.yrllanio.cursomc.domain.Categoria;
 
+
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Integer id;
+	private Integer id;	
 	private String nome;
 
 	public CategoriaDTO() {
 	}
-	
+
 	public CategoriaDTO(Categoria obj) {
 		id = obj.getId();
 		nome = obj.getNome();
+
 	}
 
 	public Integer getId() {
