b133f1b151b5f3f96fe196cc4af2d47bf2d4c5f0
yrllanio <yrlanio@gmail.com>
2020-10-16 00:37:57 -0300

    validacao sintatica com bean validation

--- src/main/java/com/yrllanio/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/yrllanio/cursomc/dto/CategoriaDTO.java
@@ -2,13 +2,20 @@ package com.yrllanio.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.yrllanio.cursomc.domain.Categoria;
 
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Integer id;	
+	private Integer id;
+	
+	@NotEmpty(message = "Prenchimento Obrigatório")
+	@Length(min=5, max=80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/com/yrllanio/cursomc/services/CategoriaService.java
+++ src/main/java/com/yrllanio/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.yrllanio.cursomc.domain.Categoria;
+import com.yrllanio.cursomc.dto.CategoriaDTO;
 import com.yrllanio.cursomc.repositories.CategoriaRepository;
 import com.yrllanio.cursomc.services.exceptions.DataIntegrityException;
 import com.yrllanio.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
