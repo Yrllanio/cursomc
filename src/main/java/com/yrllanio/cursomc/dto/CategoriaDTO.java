ac12c904ca869bd39aeba13919bcebd7ae443a60
yrllanio <yrlanio@gmail.com>
2020-10-17 12:51:25 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/yrllanio/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/yrllanio/cursomc/dto/CategoriaDTO.java
@@ -14,8 +14,8 @@ public class CategoriaDTO implements Serializable {
 
 	private Integer id;
 	
-	@NotEmpty(message = "Prenchimento Obrigatório")
-	@Length(min=5, max=80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+	@NotEmpty(message = "Prenchimento Obrigatório")	
+	@Length(min=5,  message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
